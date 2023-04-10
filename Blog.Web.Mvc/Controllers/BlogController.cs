05078eda30bea061db1989691d5538715284a7d8
Mustafa Şenyurt <msenyrt@gmail.com>
2023-04-10 16:42:53 +0300

    Controller ve View dosyalari eklendi.

---
+++ Blog.Web.Mvc/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Mvc.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Search(string query, int page)
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Blog.Web.Mvc/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Mvc.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index(int id, int page)
+        {
+            return View();
+        }
+    }
+}
